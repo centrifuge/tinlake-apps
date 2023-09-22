576dd50c66a7072012f10508014afd819d4dcead
JP Angelle <jp@k-f.co>
2023-09-22 15:15:13 -0500

    chore: add centrifuge banner

---
+++ tinlake-ui/components/ExploreCentrifugeBanner.tsx
@@ -0,0 +1,18 @@
+import { Anchor, Box, Card, Paragraph } from 'grommet'
+
+export const ExploreCentrifugeBanner = () => (
+  <Card
+    margin={{ top: 'medium' }}
+    pad={{ top: 'xsmall', bottom: 'xsmall', left: 'medium', right: 'medium' }}
+    style={{
+      backgroundColor: '#cde5ff',
+    }}
+  >
+    <Box>
+      <Paragraph>
+        Tinlake is no longer supported. Please proceed to the new Centrifuge app at{' '}
+        <Anchor href="https://app.centrifuge.io" label="app.centrifuge.io" />.
+      </Paragraph>
+    </Box>
+  </Card>
+)
