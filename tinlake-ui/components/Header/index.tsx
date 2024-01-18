41aa6a366865914e7b952b1d955c177dec67155a
JP <jp@k-f.co>
2024-01-18 12:42:23 -0600

    chore: add ExpiringCFGRewardsBanner component to Header (#41)

--- tinlake-ui/components/Header/index.tsx
+++ tinlake-ui/components/Header/index.tsx
@@ -18,6 +18,7 @@ import { useCFGRewards } from '../../utils/useCFGRewards'
 import { usePool } from '../../utils/usePool'
 import { usePortfolio } from '../../utils/usePortfolio'
 import { ClientOnlyRender } from '../ClientOnlyRender'
+import { ExpiringCFGRewardsBanner } from '../ExpiringCFGRewardsBanner'
 import { ExploreCentrifugeBanner } from '../ExploreCentrifugeBanner'
 import { RewardsBanner } from '../RewardsBanner'
 import { useTinlake } from '../TinlakeProvider'
@@ -258,6 +259,11 @@ const Header: React.FC<Props> = (props: Props) => {
         </MobileNav>
       </HeaderBar>
       {!props.hideRewardsBanner && <RewardsBanner />}
+      <Box justify="center" direction="row">
+        <Box width="xlarge">
+          <ExpiringCFGRewardsBanner />
+        </Box>
+      </Box>
       <Box justify="center" direction="row">
         <Box width="xlarge">
           <ExploreCentrifugeBanner />
